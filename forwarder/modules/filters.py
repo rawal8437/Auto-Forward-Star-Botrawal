c461ce62803c14b972bf03747631c28cb4dee6ba
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-09-17 08:34:29 +0530

    Create filters.py

---
+++ forwarder/modules/filters.py
@@ -0,0 +1,10 @@
+from forwarder import FORWARD_FILTERS
+from pyrogram.types import Message
+
+async def FilterMessage(message: Message):
+    if (message.forward_from or message.forward_from_chat) and ("forwarded" not in FORWARD_FILTERS):
+        return 400
+    if (len(FORWARD_FILTERS) == 9) or ((message.video and ("video" in FORWARD_FILTERS)) or (message.document and ("document" in FORWARD_FILTERS)) or (message.photo and ("photo" in FORWARD_FILTERS)) or (message.audio and ("audio" in FORWARD_FILTERS)) or (message.text and ("text" in FORWARD_FILTERS)) or (message.animation and ("gif" in FORWARD_FILTERS)) or (message.poll and ("poll" in FORWARD_FILTERS)) or (message.sticker and ("sticker" in FORWARD_FILTERS))):
+        return 200
+    else:
+        return 400
