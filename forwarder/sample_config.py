4817e9a707a37739183fbb2fb01622022055a760
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-09-17 08:57:41 +0530

    Update sample_config.py

--- forwarder/sample_config.py
+++ forwarder/sample_config.py
@@ -18,9 +18,10 @@ class Config(object):
     # REQUIRED
     API_KEY = "617722711:AAHysCHsVcQja6LOlHa88101lKjrxez80-M"  # API key obtained from BotFather
     OWNER_ID = "582884567"  # If you dont know, run the bot and do /id in your private chat with the bot
-    
+
+    FILES_FILTERS = "video document"
     DEFAULT_FILTERS = "video document photo audio text gif forwarded poll sticker"
-    FORWARD_FILTERS = list(set(x for x in os.environ.get("FORWARD_FILTERS", "document video").split()))
+    FORWARD_FILTERS = list(set(x for x in os.environ.get("FORWARD_FILTERS", FILES_FILTERS).split()))
     
     # FOR AUTOMATICALLY FORWARDING MESSAGES
     FROM_CHATS = [-1001234704297]  # List of chat id's to forward messages from
